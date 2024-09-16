e0344b8b63b29c1d0c2fbb5f21179c6af989f2f7
kim5647 <stepanv2005x@gmail.com>
2024-09-16 17:02:08 +0300

    update all file

---
+++ apps/VisualInterface.py
@@ -0,0 +1,13 @@
+from aiogram import Bot
+from aiogram.types import BotCommand
+
+async def set_bot_commands(bot: Bot):
+    commands = [
+        BotCommand(command="start", description="Запуск бота"),
+        BotCommand(command="addadmin", description="Добавить администратора"),
+        BotCommand(command="removeadmin", description="Удалить администратора")
+    ]
+    try:
+        await bot.set_my_commands(commands)
+    except Exception as e:
+        print(f"Error setting bot commands: {e}")
--- main.py
+++ main.py
@@ -3,14 +3,17 @@ import logging
 import config
 from aiogram import Bot, Dispatcher
 from apps.handler import rout
+from apps.VisualInterface import set_bot_commands
 
 bot = Bot(token=config.TOKEN)
 dp = Dispatcher()
 
 
 async def main():
+    await set_bot_commands(bot)
     dp.include_router(rout)
     await dp.start_polling(bot)
+
 
 
 if __name__ == "__main__":
